8de1d59553ed482019188ed65e04d66757736ea6
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-06-27 16:34:54 +0200

    creazione migration

---
+++ database/migrations/2022_06_27_142118_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('code_train', 10);
+            $table->string('agency', 50);
+            $table->string('station_departure', 50);
+            $table->string('station_arrival', 50);
+            $table->string('time_departure', 50);
+            $table->string('time_departure', 50);
+            $table->string('number_train_carriages', 50);
+            $table->boolean('delay');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('contents')
+
+<h1>Ciao</h1>
+    
+@endsection
